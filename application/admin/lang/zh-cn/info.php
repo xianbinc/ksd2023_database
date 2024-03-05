00a83884ef9a4e61ca4eac116219d237b2cc7505
xianbinc <156858081+xianbinc@users.noreply.github.com>
2024-03-05 15:53:21 +0800

    Add files via upload

---
+++ application/admin/lang/zh-cn/info.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'Id'            => 'id',
+    'Pmid'          => 'PMID',
+    'Type'          => 'Type',
+    'Therapy'       => 'Therapy',
+    'Biomarkertype' => 'BiomarkerType',
+    'Biomarker'     => 'Biomarker',
+    'Geneid'        => 'Gene ID',
+    'Symbol'        => 'Symbol',
+    'Drug'          => 'Drug',
+    'Drugbank'      => 'DrugBank ID',
+    'Target'        => 'Target',
+    'Suppressor'    => 'suppressor/driver',
+    'Expression'    => 'Expression',
+    'Detecting'     => 'DetectingTechniques',
+    'Url'           => 'Url',
+    'Journal'       => 'Journal',
+    'Year'          => 'Year',
+    'Title'         => 'Title',
+    'Authors'       => 'Authors',
+    'Keywords'      => 'Keywords'
+];
